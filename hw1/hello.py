e4d01300866bb415ba29f9cd89d6ab23c5787822
SadracUchicago <40720199+SadracUchicago@users.noreply.github.com>
2018-07-08 12:09:03 -0700

    Changed Variable Placement

--- hw1/hello.py
+++ hw1/hello.py
@@ -1,10 +1,11 @@
 Name = input("What is your name?: ")
-Color = input("What's your favorite color?")
+print("Cool. Hello", Name, "nice to meet you! ")
+
+Color = input("What's your favorite color?: ")
+
 List_Colors = ["Orange", "Black", "Pink", "Yellow"]
 
-print("Cool. Hello", Name, "nice to meet you!", Color)
-
-if List_Colors[0]:
-    print("Hey that's my favorite color too")
-
-    
+if List_Colors[0] == Color:
+    print("Hey that's my favorite color too!")
+elif Color = List_Colors[0]:
+    print("That is an absolutely horrendous color, and I cannot associate with your kind!")
