765d9052e2a284d95b861bd2bf300440d00581a4
SadracUchicago <40720199+SadracUchicago@users.noreply.github.com>
2018-07-08 11:55:18 -0700

    Hello (Challenge)

---
+++ hw1/hello.py
@@ -0,0 +1,10 @@
+Name = input("What is your name?: ")
+Color = input("What's your favorite color?")
+List_Colors = ["Orange", "Black", "Pink", "Yellow"]
+
+print("Cool. Hello", Name, "nice to meet you!", Color)
+
+if List_Colors[0]:
+    print("Hey that's my favorite color too")
+
+    
