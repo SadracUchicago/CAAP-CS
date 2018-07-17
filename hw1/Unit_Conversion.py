cd0222722da92bf9ef01d271088b768134045a48
SadracUchicago <40720199+SadracUchicago@users.noreply.github.com>
2018-07-16 19:18:01 -0700

    Update

--- hw1/Unit_Conversion.py
+++ hw1/Unit_Conversion.py
@@ -1,4 +1,11 @@
-quantity, units = input("Enter amount with units: ").split()[:2]
-conversion = eval(quantity)*{'inches': 2.54, 'centimeters': 0.393701}[units]
+print("This short program is designed to convert centimeters to inches or inches to centimeters. Whilst using it, "
+      "please include the necessary notation.")
 
+### short explanation of program
+measurement, units = input("Enter amount with units: ").split()[:2]
+
+### necessitates user input of both the numerical measurement and it's units
+
+conversion = eval(measurement) * {"inches": 2.54, "centimeters": 0.393701}[units]
+# the input, whether inches or centimeters will be designated its own operation
 print(conversion)
---
+++ hw1/fibonacci.py
@@ -0,0 +1,9 @@
+import math
+n = int(input("What nth number of the Fibonacci sequence do you want?: "))
+fibonacci = round(int((1+math.sqrt(5))**n-(1-math.sqrt(5))**n)/(2**n*math.sqrt(5)))
+
+print(fibonacci)
+
+
+
+
