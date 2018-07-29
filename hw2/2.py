3e4b5db763195be58bf61f12b3c3ddd1a42bac24
SadracUchicago <40720199+SadracUchicago@users.noreply.github.com>
2018-07-28 20:59:13 -0700

    Update

---
+++ hw2/2.py
@@ -0,0 +1,14 @@
+number_score = int(input("Enter the number score: "))
+
+if number_score == 5:
+    print("The grade is A")
+elif number_score == 4:
+    print("The grade is B")
+elif number_score == 3:
+    print("The grade is C")
+elif number_score == 2:
+    print("The grade is D")
+elif number_score == 1:
+    print("The grade is F")
+else:
+    pass
---
+++ hw2/4.py
@@ -0,0 +1,5 @@
+Credits = input("How many credits?: ")
+
+for i in Credits:
+    if i in range(0, 7):
+        print("The student is a freshman")
